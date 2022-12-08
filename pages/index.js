ecbfe07773ff6ce3466ac2141e5a547574ff1747
Guilherme Santos <gui.carv.santos@gmail.com>
2022-12-07 22:50:51 -0300

    Dev (#7)
    * solving conflicts
    
    * solving conflicts avigating-learnt (#3)
    
    * Assets metadata css (#5)
    
    * metadata learnt
    
    * script component learnt
    
    * css modules
    
    * global styles learnt
    
    * layout polished

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-
 export default function Home() {
   return (
     <Layout home>
